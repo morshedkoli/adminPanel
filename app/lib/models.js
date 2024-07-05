d097ecdee511dee41714e1ddbbbd01b0642a08de
MORSHED AL MAIN <murshedkoli@gmail.com>
2024-07-05 10:26:24 +0600

    update login live

--- app/lib/models.js
+++ app/lib/models.js
@@ -39,40 +39,40 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const productSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    desc: {
-      type: String,
-      required: true,
-    },
-    price: {
-      type: Number,
-      required: true,
-      min: 0,
-    },
-    stock: {
-      type: Number,
-      required: true,
-      min: 0,
-    },
-    img: {
-      type: String,
-    },
-    color: {
-      type: String,
-    },
-    size: {
-      type: String,
-    },
-  },
-  { timestamps: true }
-);
+// const productSchema = new mongoose.Schema(
+//   {
+//     title: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     desc: {
+//       type: String,
+//       required: true,
+//     },
+//     price: {
+//       type: Number,
+//       required: true,
+//       min: 0,
+//     },
+//     stock: {
+//       type: Number,
+//       required: true,
+//       min: 0,
+//     },
+//     img: {
+//       type: String,
+//     },
+//     color: {
+//       type: String,
+//     },
+//     size: {
+//       type: String,
+//     },
+//   },
+//   { timestamps: true }
+// );
 
 export const User = mongoose.models.User || mongoose.model("User", userSchema);
-export const Product =
-  mongoose.models.Product || mongoose.model("Product", productSchema);
+// export const Product =
+//   mongoose.models.Product || mongoose.model("Product", productSchema);
