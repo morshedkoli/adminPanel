6d5ec94ee790289589be9273c92ca11acd559406
MORSHED AL MAIN <murshedkoli@gmail.com>
2024-08-09 02:30:05 +0600

    redirect home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,14 @@
 import Link from "next/link"
+import { redirect } from "next/navigation"
 
 // TODO
 const Homepage = () => {
+
+  
+    redirect('/dashboard')
+  
+
+
   return (
     <div >
       
